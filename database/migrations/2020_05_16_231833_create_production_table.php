af7515758a22fd88456796c8b1e22de6424c5e0c
akira <akiraxx79xx@gemail.com>
2020-05-17 10:18:53 +0900

    制作案件DB制作

--- database/migrations/2020_05_16_231833_create_production_table.php
+++ database/migrations/2020_05_16_231833_create_production_table.php
@@ -14,7 +14,22 @@ class CreateProductionTable extends Migration
     public function up()
     {
         Schema::create('production', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
+            $table->increments('user_id');
+            $table->increments('contact_number');
+            $table->storing('company_name');
+            $table->storing('slug');
+            $table->storing('completed')->default('未着手');
+            $table->increments('category_id');
+            $table->storing('product_name');
+            $table->increments('numcer');
+            $table->increments('materiar_id');
+            $table->text('comment');
+            $table->storing('image');
+            $table->date('date');
+            $table->increments('shipment_location_id');
+            $table->increments('carrier_id');
+            $table->nullableTimestamps('created_at')->useCurrent();
             $table->timestamps();
         });
     }
