e34e4fb695ef9c0f46f8592f3e7b28a0bf35dc87
akira <akiraxx79xx@gemail.com>
2023-01-12 10:02:52 +0900

    app.phpにLaravel￥Cashier￥CashierServiceProvider::class追加

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         OwenIt\Auditing\AuditingServiceProvider::class,
 
+
         /*
          * Package Service Providers...
          */
@@ -175,6 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Laravel\Cashier\CashierServiceProvider::class,
 
     ],
 
@@ -226,6 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Cashier' => Laravel\Cashier\CashierServiceProvider::class,
 
     ],
 
